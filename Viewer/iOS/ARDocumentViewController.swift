c2df7a21c95ce1fb547a981c13f81026e97bb874
Aditya Rudrapatna <aditya@Adityas-MacBook-Pro.local>
2023-12-05 21:21:02 +0000

    stable AR

--- Viewer/iOS/ARDocumentViewController.swift
+++ Viewer/iOS/ARDocumentViewController.swift
@@ -10,24 +10,10 @@ import ARKit
 import SceneKit
 import UIKit
 
-class ARDocumentViewController: UIViewController {
+class ARDocumentViewController: UIViewController, ARSCNViewDelegate {
     
     var arSceneView: ARSCNView = .init()
     var model: SCNNode!
-    
-    enum AppState {
-      case DetectSurface
-      case PointAtSurface
-      case TapToStart
-      case Started
-    }
-    
-    // App state management
-    var trackingStatus: String = ""
-    var statusMessage: String = ""
-    var appState: AppState = .DetectSurface
-    
-    var statusLabel: UILabel!
     
     init(model: SCNNode) {
         self.model = model
@@ -41,22 +27,18 @@ class ARDocumentViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        // Configure the ARSCNView
         arSceneView.frame = view.bounds
-        view.addSubview(arSceneView)
         
-        // Configure label
-        statusLabel = UILabel()
-        statusLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(statusLabel)
-        NSLayoutConstraint.activate([
-            statusLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5),
-            statusLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 5),
-            statusLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -5),
-        ])
+        arSceneView.delegate = self
+        view.addSubview(arSceneView)
         
         // Create a new ARWorldTrackingConfiguration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.worldAlignment = .gravity
+        configuration.providesAudioData = false
+        configuration.planeDetection = .horizontal
+        configuration.isLightEstimationEnabled = true
+        configuration.environmentTexturing = .automatic
         arSceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
 
         // Add a tap gesture recognizer to handle surface selection
@@ -66,26 +48,31 @@ class ARDocumentViewController: UIViewController {
     
     // Handle tap on the AR scene to place all models
     @objc func handleTap(_ gesture: UITapGestureRecognizer) {
+        guard gesture.state == .ended else { return }
         let tapLocation = gesture.location(in: arSceneView)
-        if #available(iOS 13, *) {
-            let rayCastQueryResult = arSceneView.raycastQuery(from: tapLocation, allowing: .existingPlaneGeometry, alignment: .horizontal)
-            if let position = rayCastQueryResult?.origin {
-                // Set the desired size for the models (adjust as needed)
-                let targetSize: CGFloat = 0.01
-                let newNode = model.clone()
-                // Calculate the bounding box of the model
-                let (min, max) = newNode.boundingBox
-                let size = SCNVector3Make(max.x - min.x, max.y - min.y, max.z - min.z)
-
-                // Calculate the scale factor based on the target size
-                let scaleFactor = targetSize / CGFloat(size.x)
-
-                newNode.position = SCNVector3(position)
-                newNode.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
-                
-//                if arSceneView.scene.rootNode.childNodes == [] {
+        
+        // Remove existing nodes
+        arSceneView.scene.rootNode.childNodes.forEach { $0.removeFromParentNode() }
+        
+        // Get hit location
+        if let query = arSceneView.raycastQuery(from: tapLocation, allowing: .estimatedPlane, alignment: .horizontal) {
+            let results = arSceneView.session.raycast(query)
+            
+            if results.count == 1 {
+                if let match = results.first {
+                    let t = match.worldTransform
+                    let position = SCNVector3(x: t.columns.3.x, y: t.columns.3.y, z: t.columns.3.z)
+                    let targetSize: CGFloat = 0.5
+                    let newNode = model.clone()
+                    newNode.position = position
+                    // Calculate the bounding box of the model
+                    let (min, max) = newNode.boundingBox
+                    let size = SCNVector3Make(max.x - min.x, max.y - min.y, max.z - min.z)
+                    // Calculate the scale factor based on the target size
+                    let scaleFactor = targetSize / CGFloat(size.x)
+                    newNode.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
                     arSceneView.scene.rootNode.addChildNode(newNode)
-//                }
+                }
             }
         }
     }
@@ -94,45 +81,5 @@ class ARDocumentViewController: UIViewController {
         super.viewWillDisappear(animated)
         // Pause the AR session when the view controller disappears
         arSceneView.session.pause()
-    }
-}
-
-extension ARDocumentViewController: ARSCNViewDelegate {
-    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
-        DispatchQueue.main.async {
-          self.updateStatus()
-        }
-    }
-}
-
-extension ARDocumentViewController {
-    // For state management
-    func startARView() {
-      DispatchQueue.main.async {
-        self.appState = .DetectSurface
-      }
-    }
-    
-    func resetApp() {
-        DispatchQueue.main.async {
-            //self.resetARSession()
-            self.appState = .DetectSurface
-        }
-    }
-    
-    func updateStatus() {
-      switch appState {
-      case .DetectSurface:
-        statusMessage = "Scan available flat surfaces..."
-      case .PointAtSurface:
-        statusMessage = "Point at designated surface first!"
-      case .TapToStart:
-        statusMessage = "Tap to start."
-      case .Started:
-        statusMessage = "Tap objects for more info."
-      }
-
-      self.statusLabel.text = trackingStatus != "" ?
-          "\(trackingStatus)" : "\(statusMessage)"
     }
 }
